5e68feab8fd83daf9ad0c25a0e49ba37b349bdbc
Trọng Nhân <trongnhan10a11@gmail.com>
2025-06-19 10:21:41 +0700

    xong playerMovement animation

--- Assets/Scripts/PlayerMovement/PlayerController.cs
+++ Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D playerRb;
     private float HorizontalInput;
     private float VerticalInput;
+
+    public Animator playerAnim;
     [SerializeField] float moveSpeed = 5f;
     void Start()
     {
@@ -21,7 +23,21 @@ public class PlayerMovement : MonoBehaviour
         Vector2 move = new Vector2(HorizontalInput, VerticalInput).normalized;
 
         playerRb.linearVelocity = move * moveSpeed;
-        
+
+        HorizontalInput = Mathf.Clamp(HorizontalInput, -1f, 1f);
+        VerticalInput = Mathf.Clamp(VerticalInput, -1f, 1f);
+
+        playerAnim.SetFloat("MoveX", HorizontalInput);
+        playerAnim.SetFloat("MoveY", VerticalInput);
+
+        if (HorizontalInput == 0 && VerticalInput == 0)
+        {
+            playerAnim.SetBool("isMoving", false);
+        }
+        else
+        {
+            playerAnim.SetBool("isMoving", true);
+        }
     }
 
 }

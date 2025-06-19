ffb9eec9f0db44aa4dce98cd1f0393e026a934bb
DeadLine <93661423+Comotcaiten@users.noreply.github.com>
2025-06-19 22:54:40 +0700

    rpc to place bomb in move controller of player

--- Assets/Scripts/PlayerMovement/PlayerController.cs
+++ Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -11,4 +11,28 @@ public class PlayerMovement : NetworkBehaviour
 
     public Animator playerAnim;
     [SerializeField] float moveSpeed = 5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RequestPlaceBombRpc(new Vector3Int(1, 1, 1), NetworkObjectId);
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    private void RequestPlaceBombRpc(Vector3Int gridPosition, ulong sourcePlayerId)
+    {
+        // Validate tại server
+        PlaceBombRpc(gridPosition, sourcePlayerId);
+    }
+    
+    [Rpc(SendTo.ClientsAndHost)]
+    private void PlaceBombRpc(Vector3Int pos, ulong playerId)
+    {
+        // Validate hợp lệ: ô trống? cooldown xong chưa? có quyền không?
+        // Instantiate bomb prefab (NetworkObject)
+        GameObject bomb = Instantiate(bomberPrefab, pos, Quaternion.identity);
+        // bomb.GetComponent<NetworkObject>().Spawn(); // Netcode sẽ sync tới tất cả clients
+    }
 }
